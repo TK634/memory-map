bc55f477dabf418a62b44309b3fbbb3f60e4173e
tk634 <mr.taka.sueoka@gmail.com>
2026-07-19 10:58:07 +0900

    Initial Commit

---
+++ Ashiato/AshiatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AshiatoApp.swift
+//  Ashiato
+//
+//  Created by Takahiro Sueoka on 2026/07/19.
+//
+
+import SwiftUI
+
+@main
+struct AshiatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
